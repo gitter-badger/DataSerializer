cc8c453bec5b00601e82c492ade8e61bd8108b35
kulakowka <kulakowka@gmail.com>
2014-11-21 04:21:09 +0300

    test Travis

---
+++ test.js
@@ -0,0 +1,38 @@
+var assert = require("should")
+
+var Serializer = require('./index');
+
+var model = {
+  username: 'kulakowka',
+  firstname: 'Anton',
+  lastname: 'Kulakov',
+  password: 'qwerty'
+}
+
+var collection = [model, model];
+
+var rules = {
+  'username': true,
+  'fullname': function() {
+      return this.firstname + ' ' + this.lastname;    
+  }
+}
+
+var Model = Serializer(rules, model);
+
+var Collection = Serializer(rules, collection);
+
+describe('Serializer', function(){
+  it('Serializer return true data', function(){
+
+    Model.should.have.property('username', 'kulakowka');
+    Model.should.have.property('fullname', 'Anton Kulakov');
+
+    Collection.should.be.instanceof(Array).and.have.lengthOf(2);
+    Collection.map(function(user){
+      user.should.have.property('username', 'kulakowka');
+      user.should.have.property('fullname', 'Anton Kulakov');
+    });
+
+  })
+})
